912503d492a9e6405905a5a7d9cdb1335d8637bc
Gábor Szántó <szantog82@gmail.com>
2016-09-28 10:48:44 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var formidable = require('express-formidable');
+ var app = express();
+ 
+ 
+ 
+app.use(formidable.parse());
+ 
+ app.get('/',function(req,res){
+      res.sendFile(__dirname + "index.html");
+});
+ 
+app.post('/upload', function (req, res) {
+  res.send(JSON.stringify({"size": parseInt(JSON.stringify(req.body.file.size))}));
+});
+
+app.listen(process.env.PORT || 8080)
